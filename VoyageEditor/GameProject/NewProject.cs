00d3d339da1b8b253bf34e5677aece0254f9b8cc
Garrette Allen <garrettewallen@icloud.com>
2024-11-13 09:51:04 -0500

    Setup for project creation

---
+++ VoyageEditor/GameProject/NewProject.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using VoyageEditor.Utilities;
+using System.Collections.ObjectModel;
+
+namespace VoyageEditor.GameProject
+{
+    [DataContract]
+
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public required string ProjectType { get; set; }
+        [DataMember]
+        public required string ProjectFile { get; set; }
+        [DataMember]
+        public required List<string> Folders { get; set; }
+
+        public required byte[] Icon { get; set; }
+        public required byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+
+    class NewProject : ViewModelBase
+    {
+        // hardcoded path = bad, get from install location?
+        private readonly string _templatePath = @"..\..\VoyageEditor\ProjectTemplates\";
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            }
+        }
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\VoyageProjects\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public  ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        { get;  }
+
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // error logging not just debug output
+            }
+        }
+    }
+}
